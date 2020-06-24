b63f5f0a3a4e8bac91553af6406aa71d12eef541
Marcelo Fernandes de Souza <fernands@outlook.com>
2020-06-24 11:29:58 -0300

    Update partial_linear_plot.R

--- code/partial_linear_plot.R
+++ code/partial_linear_plot.R
@@ -1,11 +1,8 @@
 # plot Brasil
 covidbr %>%
-<<<<<<< HEAD
   mutate(aux = ifelse(semanaEpi == 26 | semanaEpi < 22, NA, obitos2a7),
          semanaEpi = ifelse(semanaEpi == 26 | semanaEpi < 22, NA, semanaEpi)) %>% 
-=======
   mutate(aux = obitos2a7) %>% 
->>>>>>> 4b3484a81494b49ef9fa39bf4000fd39cf10e5f5
   filter(occday >= 21) %>% 
   ggplot() + 
   geom_line(aes(x = data, y = obitosAcumulado), color = "green", cex = 1.1) +
@@ -15,7 +12,6 @@ covidbr %>%
   geom_dl(aes(label = semanaEpi, x = data, y = aux), 
           method = list(dl.trans(x = x -.35, y = y + .47), "last.bumpup")) +
   geom_point(aes(x = data, y = aux)) +
-<<<<<<< HEAD
   ylim(0, 56000) +
   scale_x_datetime(limits = ymd_h(c("2020-03-15 00", "2020-06-25 23"))) +
   # geom_smooth(aes(x = data, y = aux),
@@ -26,7 +22,6 @@ covidbr %>%
 #               labels = trans_format("log10", math_format(10^.x))) +
 coord_cartesian(ymd_h(c("2020-05-25 00", "2020-06-25 23")),
                 ylim = c(20000, 56000))
-=======
   ylim(0, 23000) +
   scale_x_datetime(limits = ymd_h(c("2020-03-15 00", "2020-05-23 23"))) +
   geom_smooth(aes(x = data, y = aux),
@@ -34,7 +29,6 @@ coord_cartesian(ymd_h(c("2020-05-25 00", "2020-06-25 23")),
               method.args = list(family = gaussian(link = 'log'))) 
 # scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
 #               labels = trans_format("log10", math_format(10^.x)))
->>>>>>> 4b3484a81494b49ef9fa39bf4000fd39cf10e5f5
 
 # plot São Paulo state
 covidsp %>%
